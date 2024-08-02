ff2123bfa33a7a1e460eab9352920eef4466d50d
artur <artur@MacBook-Pro-artur.local>
2024-08-02 13:29:46 +0500

    Added thread into run.py for method converting_files

--- Controller.py
+++ Controller.py
@@ -1,6 +1,7 @@
 from pdf2image import convert_from_path
 from tqdm import tqdm
 import os
+from datetime import datetime
 
 PATH_TO_LIB=os.path.join(os.path.dirname(__file__),"Library") # Только для Windows
 PATH_FILE_OUT = os.path.join(os.path.dirname(__file__),"out")
@@ -10,21 +11,22 @@ class Controller():
     def __init__(self):
         pass
 
-    def start(self, list_path: list) ->None: 
+    def converting_fl(self, list_path: list) ->None: 
         '''Запуск процесса конвертирования и вывод на экран вспомогательных сообщений.
         По завершению открывает папку с файлами.
         '''         
         print(f"Всего pdf-файлов: {len(list_path)}")     
         print("Логирование конвертации:\n")
         for i, path in enumerate(list_path,1):
+            dt = datetime.now().strftime("%d.%m.%Y_%H_%M_%S")
             print(f"№{i} Конвертируется файл: {path}")
-            self.converting(i, path) # запуск функции конвертация
-            print(f'Конвертация файла завершена, создан следующий файл: out{i}.tiff')
+            self.create_tiff_files(dt, path) # запуск функции конвертация
+            print(f'Конвертация файла завершена, создан следующий файл: out_{dt}.tiff')
             print("--------------------------------------------------------------------------------------")
         print("\nКонвертация завершена, после закрытия логирование будет удалено.") 
         self.startfine()
     
-    def converting(self, index: int, path: str) -> None:        
+    def create_tiff_files(self,dt: str, path: str) -> None:        
         '''Запуск процесса ковертирования, на входе идентификатор списка и его путь,
         после превращает pdf в массив картинок, складывает все картинки в необходимый
         формат tiff, на выходе показывает прогресс бар и конвертированный файл.
@@ -32,7 +34,7 @@ class Controller():
         images = self.get_images(path, PATH_TO_LIB) 
         progress_bar = tqdm(images,desc="Прогресс конвертации:",total=len(images))
         for i in range(len(images)):
-            images[0].save(os.path.join(PATH_FILE_OUT, f"out{index}.tiff"),save_all=True,append_images=images[1:],compression='tiff_lzw') 
+            images[0].save(os.path.join(PATH_FILE_OUT, f"out_{dt}.tiff"),save_all=True,append_images=images[1:],compression='tiff_lzw') 
             progress_bar.update() 
         progress_bar.close()
 
--- Run.py
+++ Run.py
@@ -1,8 +1,10 @@
 
 '''Импорт Qt libs для формирования GUI и обработчиков событий'''
 from PyQt6 import QtWidgets
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QMessageBox
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+import threading
 
 import sys, os
 '''Импорт основного контроллера и UI'''
@@ -13,26 +15,24 @@ ICON = os.path.join(os.path.dirname(__file__), "Resources","icon.png")
 ABOUT = os.path.join(os.path.dirname(__file__),"Resources","about.txt")
 PATH_FILE_OUT = os.path.join(os.path.dirname(__file__),"out")
 
-
 class App(QtWidgets.QMainWindow, UI.Ui_MainWindow, Controller):
     list_path=[]
     def __init__(self) ->None:
         super().__init__()    
         self.setWindowIcon(QIcon(ICON)) 
         self.setupUi(self)   
-
         '''Обработчики кнопок'''      
         self.pButtonPath.clicked.connect(self.browse_folder)
-        self.pButtonStart.clicked.connect(self.convert_start)    
+        self.pButtonStart.clicked.connect(self.thread_converting_files)    
         
         '''Обработчики меню'''
         self.menu_action_path.triggered.connect(self.browse_folder)    
-        self.menu_action_start.triggered.connect(self.convert_start)
+        self.menu_action_start.triggered.connect(self.thread_converting_files)
         self.menu_action_pdf.triggered.connect(self.open_folder_pdf)    
         self.menu_action_tiff.triggered.connect(self.open_folder_tiff)
         self.menu_about.triggered.connect(self.about_show)    
         self.menu_version.triggered.connect(self.version_show)
-
+        
     def browse_folder(self) ->None:
         '''Просмотр выбранной папки'''
         self.labelPath.clear()
@@ -43,16 +43,21 @@ class App(QtWidgets.QMainWindow, UI.Ui_MainWindow, Controller):
             self.list_path= self.get_path_to_pdf_files(directory, self.checkBoxRec.isChecked())
             text = f"Найдено: {len(self.list_path)} pdf-файлов"
             self.labelPath.setText(directory)
-            self.labelFound.setText(text)        
+            self.labelFound.setText(text) 
 
-    def convert_start(self) ->None:
+    def thread_converting_files(self):
+        th = threading.Thread(target=self.converting_files)
+        th.start()
+        th.join()
+      
+    def converting_files(self):
         '''Запускает конвертацию блокируя основной поток с учетом всех необходимых проверок
         Основной поток не нужен, т.к. в процессе работы к нему обращатся не будут'''
-        path = self.labelPath.text()
+        path = self.labelPath.text()   
         if path:
             if os.path.exists(path):
                 if self.list_path:
-                    self.start(self.list_path)
+                    self.converting_fl(self.list_path)
                 else: self.message_show(title="Ошибки в конвертации.",text="В папке pdf-файлы не найдены!")
             else: self.message_show(title="Ошибки в конвертации.",text="Папка не существует!")
         else: self.message_show(title="Ошибки в конвертации.",text="Не выбран путь к pdf-файлам!")
